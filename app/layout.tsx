761b832fd899a686aef5533e80f27308d7519358
José <clasesvirtualez96@gmail.com>
2026-05-30 21:10:00 -0500

    implementacion de tag de google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import Header from "@/src/components/modules/Header";
 import FloatingConnect from "@/src/components/modules/FloatingConnect";
@@ -140,6 +141,19 @@ export default function RootLayout({
         />
       </head>
       <body className="antialiased flex flex-col min-h-screen">
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-NPQH2R8ETN"
+        />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-NPQH2R8ETN');
+          `}
+        </Script>
         <Header />
         {children}
         <FloatingConnect />
